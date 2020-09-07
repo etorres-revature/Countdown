e415bd24f0569eb3da15827e442aca7287c91efa
Eric D. Torres <etorresnotary@gmail.com>
2020-09-07 12:58:13 -0500

    countdown completed

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const secondsEl = document.querySelector("#seconds");
 const countdownEl = document.querySelector("#countdown");
 
 const currentYear = new Date().getFullYear();
+console.log(currentYear);
 
-const newYeartime = new Date(`January 01 ${currentYear + 1}00:00:00`);
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+console.log(newYearTime);
+
 
 function updateCountdown() {
   const currentTime = new Date();
-  const diff = newYeartime - currentTime;
+  const diff = newYearTime - currentTime;
 
   // console.log(diff);
 
@@ -20,9 +23,14 @@ function updateCountdown() {
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   console.log(h);
   const m = Math.floor(diff / 1000 / 60) % 60;
-  console.log(minutesEl);
+  console.log(m);
   const s = Math.floor(diff / 1000) % 60;
   console.log(s);
+
+  daysEl.innerHTML = d;
+  hoursEl.innerHTML = h < 10 ? "0" + h : h;
+  minutesEl.innerHTML = m < 10 ? "0" + m : m;
+  secondsEl.innerHTML = s < 10 ? "0" + s : s;
 }
 
 setInterval(updateCountdown(), 1000);
